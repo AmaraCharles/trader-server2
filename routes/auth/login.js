a9c3ec13ec36e6ee002160a17f0fb29dd4b5789f
ninvincible <73164451+AmaraCharles@users.noreply.github.com>
2024-04-13 19:25:39 +0100

    Update login.js

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -4,7 +4,7 @@ const UsersDatabase = require("../../models/User");
 var router = express.Router();
 
 router.post("/login", async function (request, response) {
-  const { Id} = request.body;
+  const { id} = request.body;
   /**
    * step1: check if a user exists with that email
    * step2: check if the password to the email is correct
@@ -12,7 +12,7 @@ router.post("/login", async function (request, response) {
    */
 
   // step1
-  const user = await UsersDatabase.findOne({ id: Id });
+  const user = await UsersDatabase.findOne({ id: id });
 
   if (user) {
     // step2
